3191357c3b36f16205b190d0233826d2402c22d9
Raviphp1234 <89478888+Raviphp1234@users.noreply.github.com>
2021-08-25 09:58:31 +0530

    today
    ravi

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,109 @@
+<?php
+  
+namespace App\Http\Controllers;
+   
+use App\Models\Student;
+use Illuminate\Http\Request;
+  
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::latest()->paginate(5);
+    
+        return view('student.index',compact('students'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+     
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('student.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+        ]);
+    
+        Student::create($request->all());
+     
+        return redirect()->route('student.index')
+                        ->with('success','Student created successfully.');
+    }
+     
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        return view('student.show',compact('student'));
+    } 
+     
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        return view('student.edit',compact('student'));
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+        ]);
+    
+        $student->update($request->all());
+    
+        return redirect()->route('student.index')
+                        ->with('success','student updated successfully');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+    
+        return redirect()->route('student.index')
+                        ->with('success','student deleted successfully');
+    }
+}
---
+++ storage/framework/views/d3ea55019345bc394c618375b85a3674d1c9e08c.php
@@ -0,0 +1,56 @@
+
+   
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="<?php echo e(route('student.index')); ?>"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <?php if($errors->any()): ?>
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li><?php echo e($error); ?></li>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+  
+    <form action="<?php echo e(route('student.update',$student->id)); ?>" method="POST">
+        <?php echo csrf_field(); ?>
+        <?php echo method_field('PUT'); ?>
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="<?php echo e($student->name); ?>" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <textarea class="form-control" style="height:150px" name="email" placeholder="Email"><?php echo e($student->email); ?></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Address:</strong>
+                    <textarea class="form-control" style="height:150px" name="address" placeholder="Address"><?php echo e($student->address); ?></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('student.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\blog\resources\views/student/edit.blade.php ENDPATH**/ ?>
